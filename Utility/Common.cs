9e691003da75f58117400a5eff57e878a97b50e2
rongqin.lin <rongqin.lin@chukou1.com>
2017-05-26 16:57:21 +0800

    modify Utility Common

--- Utility/Common.cs
+++ Utility/Common.cs
@@ -26,7 +26,7 @@ namespace Utility
             }
             catch (Exception ex)
             {
-                LogHelper.LogException("mapper object fail ", ex);
+                LogHelper.LogException("mapper object fail ！", ex);
             }
             return d;
         }
